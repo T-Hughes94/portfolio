02626577f9eda658b2f284477fff56d6efde3a65
Terence Hughes <Terencehughes450@gmail.com>
2024-01-05 05:25:39 -0500

    filled in skills component

---
+++ new-portfolio/Components/Skills.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Skills() {
+  return (
+    <section>
+    <h2>Skills</h2>
+    <ul>
+      <li>JavaScript (ES6+)</li>
+      <li>React.js</li>
+      <li>Node.js</li>
+      <li>Python</li>
+      <li>Flask</li>
+      <li>SQLalchemy</li>
+      <li>ChatGPT</li>
+      {/* Add more skills as needed */}
+    </ul>
+  </section>
+  )
+}
+
+export default Skills
