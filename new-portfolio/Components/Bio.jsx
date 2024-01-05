d9c64f1220bb416cab7b87a33a5a7bdf43e12830
Terence Hughes <Terencehughes450@gmail.com>
2024-01-05 05:29:50 -0500

    changed bio

--- new-portfolio/Components/Bio.jsx
+++ new-portfolio/Components/Bio.jsx
@@ -6,7 +6,7 @@ function Bio() {
     <section>
     <h2>Bio</h2>
     <p>
-      Hello! I am Terence Hughes, a passionate Software Engineer with a focus on Web Development and using A.I
+      Hello! I am Terence Hughes, Junior Software Engineer with a focus on Web Development and using A.I
       to develop systems and applications that solve real world problems.
     </p>
     <img src={'/web-(1).jpg'} alt="Terence Hughes" />
