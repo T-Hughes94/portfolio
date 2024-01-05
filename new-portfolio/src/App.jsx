5d292d88cb5ab6ddefc3dae74a0b24f4251ca6a9
Terence Hughes <Terencehughes450@gmail.com>
2024-01-05 05:28:07 -0500

    wired up components at the app level

--- new-portfolio/src/App.jsx
+++ new-portfolio/src/App.jsx
@@ -1,14 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Bio from '/components/Bio';
+import Skills from '/components/Skills';
+import Projects from '/components/Projects';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      Terences Website
+        <div className="App">
+      <header>
+        <h1>Terence Hughes - Software Engineer</h1>
+      </header>
+      <main>
+        <Bio />
+        <Skills />
+        <Projects />
+      </main>
+      <footer>
+        {/* Add your contact information or social media links */}
+        <p>Contact: your.email@example.com</p>
+      </footer>
+    </div>
     </>
   )
 }
